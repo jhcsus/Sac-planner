42e8b89ade1ec61b4e502363ca357809382e1933
Nikayel Jamal <65195892+Nikayel@users.noreply.github.com>
2024-12-02 16:05:27 -0800

    Managing advisors in the degree.java

--- Degree.java
+++ Degree.java
@@ -6,6 +6,7 @@ public class Degree {
     // Constructor to initialize the requiredClasses list
     public Degree() {
         requiredClasses = new ArrayList<>();
+        advisors = new ArrayList<>();
     }
 
     public void addClass(Class newClass) {
@@ -17,4 +18,15 @@ public class Degree {
             System.out.println("Class Name: " + c.getClassName() + ", Required Grade: " + c.requiredGrade());
         }
     }
+    public void listAdvisors(){
+        //Littel edge case
+        if (advisors.isEmpty()){
+            System.out.println("No advisors!")
+        }
+        else{
+            for(Advisor advisor : advisors){
+                System.out.println("Advisors Nam:"+advisor.getName() + ",Email: " +advisor.getEmail());
+            }
+    }
+}
 }
